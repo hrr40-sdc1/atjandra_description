f96d0b459794b28b00ef693adab88f33cad62c4b
AndrewTjandra <andrew.tjandra@gmail.com>
2019-09-13 13:51:08 -0700

    Tested to create 10M primary records

--- database/DataGenerator/createHouseData.js
+++ database/DataGenerator/createHouseData.js
@@ -1,36 +1,37 @@
 const fs = require('fs');
 const faker = require('faker');
 
-const generateFile = (filepath, length) => {
+const generateHouseData = (filepath, length) => {
   return new Promise((resolve, reject) => {
-    const writeStream = fs.createWriteStream(filepath, { encoding: 'utf8' });
-    writeStream.write('[');
-    for (let i = 1; i <= length; i++) {
-      // console.log(i);
-      const obj = {
-        'title': faker.lorem.sentences(2),
-        'location': faker.address.city(),
-        'is_entire_place': faker.random.boolean(),
-        'super_host_name': `${faker.name.firstName()} ${faker.name.lastName()}`,
-        'super_host_photo': 'super-host-photo-1.jpg',
-        'rating': faker.random.number(100),
-        'desc': faker.lorem.sentences(3),
-        'space_desc': faker.lorem.sentences(10)
-      };
 
-      writeStream.write(`${JSON.stringify(obj, null, 2)}`);
-      if (i !== length) {
-        writeStream.write(',');
+    const writeStream = fs.createWriteStream(filepath, { encoding: 'utf8' });
+    writeStream.write('title,location,is_entire_place,super_host_name,super_host_photo,rating,desc,space_desc\n');
+
+    (async() => {
+      for (let i = 1; i <= length; i++) {
+        const title = faker.lorem.sentences(2);
+        const location = faker.address.city();
+        const is_entire_place = faker.random.boolean();
+        const super_host_name = `${faker.name.firstName()} ${faker.name.lastName()}`;
+        const super_host_photo = 'super-host-photo-1.jpg';
+        const rating = faker.random.number(100);
+        const desc = faker.lorem.sentences(3);
+        const space_desc = faker.lorem.sentences(10);
+
+        if(!writeStream.write(`${title},${location},${is_entire_place},${super_host_name},${super_host_photo},${rating},${desc},${space_desc},`)) {
+          await new Promise(resolve => writeStream.once('drain', resolve));
+        }
+        if (i !== length) {
+          writeStream.write('\n');
+        }
       }
-    }
-    writeStream.write(']');
-    writeStream.end();
-    writeStream.on('finish', () => { resolve(`done with ${filepath}`); });
-    writeStream.on('error', reject);
+      writeStream.end();
+    })();
+    writeStream.on('finish', () => resolve('house data generated'));
   });
 };
 
-generateFile('./description/database/DataGenerator/houses.json', 1000000)
-  .then(() => {
-    console.log('all done');
+generateHouseData('./description/database/DataGenerator/houses1.csv', 1000)
+  .then((res) => {
+    console.log(res);
   });
