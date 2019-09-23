9980aeb75da06751063a9a69b8b7bda85f1b6ebf
AndrewTjandra <andrew.tjandra@gmail.com>
2019-09-23 16:07:50 -0700

    Update connection for cassandra to take env variable, default to aws instance

--- database/cassandra/connection.js
+++ database/cassandra/connection.js
@@ -1,6 +1,8 @@
 const cassandra = require('cassandra-driver');
 
-const client = new cassandra.Client({ contactPoints: ['127.0.0.1'], localDataCenter: 'datacenter1', keyspace: 'housemania' });
+const cassandraContactPoint = process.env.CASSANDRA_IP || 'ec2-54-202-212-27.us-west-2.compute.amazonaws.com';
+
+const client = new cassandra.Client({ contactPoints: [cassandraContactPoint], localDataCenter: 'datacenter1', keyspace: 'housemania' });
 
 client.connect((err) => {
   err ? console.log(err) : console.log('connected to cassandra');
